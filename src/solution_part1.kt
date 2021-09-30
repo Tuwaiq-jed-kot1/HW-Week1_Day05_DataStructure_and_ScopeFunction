393f0c78901b2ca5470364f577ccbda771d6a84e
NadiaAliAlmrashi <Nadia000333666>
2021-10-01 02:30:19 +0300

    good

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,3 +1,36 @@
-fun main(){
+ fun main() {
+    //###_____________________Scope Functions.____________________________###
+    //Create a working example withing 'Let'
+    //"Let" Execute from operation (within block), value is passed as an argument
+    // and then returns the output of this execution and save
+
+
+    val UpperCaceString = listOf("ali", "mohammed", "nadia").first().let {
+
+        it.uppercase()
+    }
+    println("the first element in list : $UpperCaceString")
+
+
+    //Create a working example withing 'Run'
+    val string = listOf("ali", "mohammed", "nadia").run {
+
+        this.first()
+        println(this)
+    }
+    println(string) //does not save operations "run"
+
+
+    //Create a working example withing 'Also'
+
+    val str = "abc"
+
+    val result = str.also {
+
+        println(it + "\u00A9") // => abc©
+
+    }
+    println(result)//save just contain "str" val not save operation "also" //print:abc
+
 
 }
--- src/solution_part3.kt
+++ src/solution_part3.kt
@@ -1,3 +1,21 @@
-fun main(){
+Write an answer to this question:
+What are the cases that I need to use the map for??
+//______??
+The kotlin map is one of the features, and it’s the generic collection interface.
+It is used for to store the elements in the key-value pairs
+like:
+Information students in school
 
-}
+
+
+What is the difference between immutable and read only variables?
+//_______??
+Mutable means that the variable can be reassigned to a different value after initial assignment.
+Read-only means that the variable cannot be reassigned once initialized.
+
+
+
+
+Create a map of 20 elements of strings and doubles
+//_______??
+    var map = HashMap<String,double >(20)
