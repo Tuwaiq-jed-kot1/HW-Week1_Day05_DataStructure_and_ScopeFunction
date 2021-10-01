2792e8160798904163aaed1adf3bfc7fb67cd935
SuzanneSaleem1 <91476799+SuzanneSaleem1@users.noreply.github.com>
2021-10-01 17:02:20 +0300

    Update solution_part3.kt

--- src/solution_part3.kt
+++ src/solution_part3.kt
@@ -6,9 +6,26 @@ fun main(){
 // These can be freely combined.o make sure that a property is not changed in any way,
 // it should be a readonly reference to an immutable object
     //Create a map of 20 elements of strings and doubles
-   val student =  mapOf<Int,String>(
-       1 to "Ahmed"
-   2 to
-
+   val student =  mapOf<Double,String>(
+       1.2 to "Ahmed"
+   2.2 to "kahled"
+      3.3 to "mohamed"
+      5.6 to "hind"
+      7.1 to "Omar"
+      84.0 to "Lama"
+      65 to "Foaad"
+      4.7 to "Muhanad"
+       33.4 to "Salim"
+       8.7 to "Hady"
+      54.0 to "Fady"
+      76.6 to "layla"
+      67.3 to "Raed"
+      66.1 to "Majid"
+      89.8 to "noor"
+      78.9 to "nabeel"
+      55.0 to "Faisel"
+      32.3 to "Yasser"
+      76.0 to "Turky"
+      56.4 to "Abdullah" 
    )
 }
