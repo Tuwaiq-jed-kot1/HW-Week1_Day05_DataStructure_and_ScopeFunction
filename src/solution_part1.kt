9de6cd1e8c22e3563316f6e7f3ce47320d215051
Bashayer Alosaimi <bashayer.alosaimi37@gmail.com>
2021-10-01 16:25:04 +0300

    I'm so sorry for the delay, it won't happen again I hope you accept my HW.

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,3 +1,45 @@
-fun main(){
 
+
+/* Part I - with Scope Functions.
+---
+- Create a working example withing 'Let'
+- Create a working example withing 'Run'  (Optional to do = Bonus!!)
+- Create a working example withing 'Also'  (Optional to do = Bonus!!)
+*/
+
+fun main() {
+
+
+    // example withing 'Let'
+    println("Example withing 'Let'\n")
+    val y = mutableListOf(1, 2, 3, 4, 5).let {
+        println("My List : $it")
+        it.add(6)
+        it.remove(1)
+        println("My new List after add 6 and remove 1 $it \n")
+    }
+
+    println("----------------------------------------------------")
+    println("Examples withing 'Run'")
+
+    //examples withing 'Run'
+
+    val fulName = mutableListOf("Bashayer", "Hasan", "Alosaimi").run {
+        this[2] = "no middle name"
+        println(this)
+    }
+    println(fulName)
+
+
+    //example withing 'Also'
+    println("----------------------------------------------------")
+    println("Example withing 'Also'\n")
+
+    val number2 = mutableListOf("one", "two", "three")
+    number2.also {
+        println(" list  before adding new one: $it")
+        number2.add("four")
+        println("after adding $number2")
+
+    }
 }
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,72 @@
-fun main(){
+/*
+*
+* ## Part II - Practice with Lists and Sets.
+---
+- Create a list then add repetitive and unique elements to it, then convert it to set, then add new elements to it and print the set values
+*/
+
+
+fun main() {
+     println("- Create a list and access it and print it, then update or add elements to it")
+println()
+    println("Example withing 'Let'\n")
+    val MyList = mutableListOf("Apple", "Orange")
+    println("My List : $MyList")
+    MyList.add("Banana")
+    MyList.remove("Apple")
+    println("My new List $MyList \n")
+
+//- Create a list and access out of its bounds, then manage the array errors safety with 3 different ways
+
+println()
+
+    println("--------------------------------------------------------------")
+
+    println("- Create a list and access out of its bounds, then manage the array errors safety with 3 different ways")
+
+
+    val myList = mutableListOf(1,2,3,4,5)
+    println(myList.getOrElse(100){ " It is out of bounds" })
+
+    println("-----------")
+
+    println(" #2 manage errors safety")
+    var choice2 : Int? = null
+    if (choice2 != null ){
+        choice2= myList[5]
+        println("you choice is $choice2")
+    } else{
+        println("$choice2  your choice is out of bounds")
+    }
+
+    println("----------")
+
+    println(" #2 manage errors safety")
+
+
+    var choice3 : Int? = null
+    try {
+        choice3= myList[100]
+    } catch (e: Exception){
+        println("$choice2  your choice is out of bounds")
+        println(e.message)
+    }
+    finally {
+        println("Enter a new choice")
+    }
+
+println()
+    println("--------------------------------------------------------------")
+
+    println("- Create a list then add repetitive and unique elements to it, then convert it to set, " +
+            "then add new elements to it and print the set values\n")
+
+    val myList2 = listOf(1,2,3,4,5,6).toMutableSet()
+    myList2.add(4)
+    println(myList2) // it will not accept duplicate
+
+
+
 
 }
+
