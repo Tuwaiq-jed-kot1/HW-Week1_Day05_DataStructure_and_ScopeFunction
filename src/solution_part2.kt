0958f542b7cbaff33e70f50c1be332aefab26ed6
SuzanneSaleem1 <Suzan.Alluhaybi.jed.k1@tuwaiq.edu.sa>
2021-10-01 16:01:22 +0300

    I coudn't finsh, short time :(

--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,20 @@
-fun main(){
+fun main() {
+    val animals = listOf("cat", "dog", "frog", "lion", "wolf")
+    println(animals)
+    var animalAdd = mutableListOf("cat", "dog", "frog", "lion", "wolf")
+    animalAdd.add("bird")
+    println(animalAdd)
 
+    try {
+
+    } catch (e: IndexOutOfBoundsException) {
+    val numbers = mutableListOf(1, 1, 3, 5, 7, 8, 9)
+    numbers[10]
+    println(numbers)
+}
+    val numbers= mutableListOf(1, 1, 3, 5, 7, 8, 9)
+    println(numbers.getOrElse(10) { "Out of band" })
+
+val newList = mutableListOf("33","6","8","33","4","8","5","7").toSet
+    println(newList)
 }
--- src/solution_part3.kt
+++ src/solution_part3.kt
@@ -1,3 +1,14 @@
 fun main(){
+// Write an asnwer to this question: What are the cases that I need to use the map for??
+   // list of class studuent, dictionary words, store any data like in Exel
+    //What is the difference between immutable and read only variables?
+    //readonly is set on the reference to an object. Immutability is a property of the object itself.
+// These can be freely combined.o make sure that a property is not changed in any way,
+// it should be a readonly reference to an immutable object
+    //Create a map of 20 elements of strings and doubles
+   val student =  mapOf<Int,String>(
+       1 to "Ahmed"
+   2 to
 
+   )
 }
