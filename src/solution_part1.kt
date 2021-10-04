01d7cfab0e749dd9851c77fea226ac3ecec322a7
Randah <randah.Almajnooni.jed.k1@tuwaiq.edu.sa>
2021-10-04 22:55:46 +0300

    fix by email cuz i used a wrong email

--- src/solution_part1.kt
+++ src/solution_part1.kt
@@ -1,3 +1,34 @@
-fun main(){
+//  SCOPE FUNCTION PRACTICE :
+data class Citizen(var name: String, var age: Int, var residence: String)
 
+fun main (){
+    val citizen1:Citizen? = Citizen("ahmad",  30 , "jeddah", )
+    val citizen2 =Citizen("mohamed", 24, "makkah", )
+
+
+
+    print("\n ــــــــ run fun ــــــــ")
+    // scoping fun : run
+    val total1 = citizen1?.run {
+        println("\n"+" age of ${this.age} - ${age}")
+        this.name.length+age+this.residence.length
+    }
+    print("\n"+total1)
+
+    print("\n ــــــــ Let fun ــــــــ")
+
+    val total2 = citizen2.let {
+        println("\n"+"age of ${it.age}")
+        it.age + it.residence.length + it.name.length
+        // let return the last line of lambda
+    }
+    print(total2)
+
+
+    print("\n ــــــــ Let also ــــــــ")
+
+    val  citizenNew:Citizen =citizen2.also {
+        it.age = 40
+    }
+    println("\n"+"new citizen age : ${citizenNew.age}")
 }
--- src/solution_part2.kt
+++ src/solution_part2.kt
@@ -1,3 +1,56 @@
+
 fun main(){
+    val mylist:MutableList<Int> = mutableListOf(1,2,3,4,5)
+    val otherList:List<Int> = mylist.toList()
+
+    mylist[0] = 7
+
+    println(mylist) // the changed has been add to the mylist
+    println(otherList)// dose not contain any of the changes afterwords
+
+    println("ــــــــــــــــــــ")
+
+
+    // method in list to add and print out the list
+    val lists:MutableList<String> = mutableListOf("apple","orange","mango")
+    lists+="kiwi"
+    print(lists[1]+"\n")
+    print(lists)
+
+    // method 1 of Bound
+    lists.getOrElse(6){
+        println("\n" + "there is no index $it") //default value in cases when the index is out of bound
+    }
+
+    print( "\n" + "ــــــــــــــــــــ")
+
+    // method 2 of Bound
+
+    val list:String? =lists.getOrNull(5) //nullable method // the output null
+    print("\n"+ list)
+
+    print("\n"+"ــــــــــــــــــــ")
+
+    // method 3 of bound
+
+    val cities: List<String?> = listOf("Kolkata", null, "Mumbai")
+    var name: List<String?> = emptyList()
+    for (city in cities) {
+        city?.let { name = name.plus(it) }
+    }
+    print("\n"+name)
+
+
+    print("\n"+"ــــــــــــــــــــ")
+
+    // repetitive and unique element and convert ot to set
+
+    var numbers:List<Int> = mutableListOf(1,2,1,3,4)
+    numbers+= 5
+    var myset = numbers.toSet()
+
+    println("\n"+"set : ${myset}")
+
+
 
 }
